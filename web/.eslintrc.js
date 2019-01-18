46f0c43601490be950c7672ee30fd300ad840c6b
ooookai <ooookai@gmail.com>
2019-01-18 18:48:20 -0500

    init: clean and lint

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -1,14 +1,46 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
-  extends: ["plugin:vue/essential", "@vue/prettier"],
+  plugins: ['vue'],
+  extends: ['plugin:vue/recommended', '@vue/prettier'],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    'no-console': 'off',
+    'no-debugger': 'off',
+
+    'comma-dangle': ['warn', 'only-multiline'],
+
+    'prettier/prettier': [
+      'warn',
+      {
+        trailingComma: 'es5',
+        singleQuote: true,
+        semi: false,
+      },
+    ],
+
+    'vue/html-indent': ['warn', 2],
+    'vue/html-self-closing': 'off',
+
+    'vue/no-use-v-if-with-v-for': 'off',
+    'vue/no-unused-components': 'off',
+
+    'vue/attributes-order': 'error',
+    'vue/order-in-components': 'error',
+    'vue/max-attributes-per-line': [
+      2,
+      {
+        singleline: 5,
+        multiline: {
+          max: 3,
+          allowFirstLine: true,
+        },
+      },
+    ],
   },
   parserOptions: {
-    parser: "babel-eslint"
-  }
-};
+    parser: 'babel-eslint',
+    ecmaVersion: 6,
+  },
+}
