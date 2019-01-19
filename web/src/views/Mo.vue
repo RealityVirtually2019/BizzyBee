98a2cad0a6f804e8bef16b2fe3be7fb9141c0381
Mo Kakwan <wavewash@gmail.com>
2019-01-18 21:37:44 -0500

    added Mo test env for flowers

--- web/src/views/Mo.vue
+++ web/src/views/Mo.vue
@@ -1,10 +1,19 @@
 <template>
-  <a-scene> </a-scene>
+  <a-scene>
+    <!-- <a-sphere position="0 1.25 -5" radius="3.5" color="#EF2D5E"></a-sphere> -->
+    <a-sky color="#ECECEC"></a-sky>
+    <Flower :stage="0" />
+  </a-scene>
 </template>
 
 <script>
 // @ is an alias to /src
+import Flower from '@/components/Flower'
+
 export default {
-  name: 'Home',
+  name: 'Mo',
+  components: {
+    Flower,
+  },
 }
 </script>
