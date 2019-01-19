7c9c46efea35d10e5ba045e85b1faf90c23ea0bd
Mo Kakwan <wavewash@gmail.com>
2019-01-19 10:50:23 -0500

    trying to get model loading to work - uploading the models I was working with

--- web/src/components/Flower.vue
+++ web/src/components/Flower.vue
@@ -1,10 +1,18 @@
 <template>
   <a-entity>
+    <!-- Using the asset management system. -->
+    <a-entity scale="0.2 0.2 0.2" position="0 1.4 -5">
+      <a-obj-model class="flowermodel" src="#bromeliads-obj" mtl="#bromeliads-mtl"></a-obj-model>
+      <a-gltf-model class="gltfmodel" src="#papatest-gltf"></a-gltf-model>
+    </a-entity>
+
+
     <a-sphere
       v-if="stage == 0"
-      position="0 1.25 -15"
+      position="0 1.25 -5"
       radius="0.5"
       color="#FF00FF"
+      material="side: double; color: #EF2D5E; transparent: true; opacity: 0.5"
     ></a-sphere>
     <a-sphere
       v-if="stage == 1"
@@ -37,6 +45,24 @@ export default {
       default: 0,
     },
   },
+  mounted() {
+    console.log(this.$el.getElementsByClassName("gltfmodel")[0]) // I'm text inside the component.
+
+     let base = document.querySelector('a-assets')
+     let model = this.$el.getElementsByClassName("gltfmodel")
+
+     base.addEventListener('loaded', function() {
+        console.log('loaded');
+        if(model != undefined) {
+          let bloomBone = model.object3D.children[0].children.filter(mesh => mesh.name == "Papa")
+          if(bloomBone) {
+            console.log("Bloom Bone - ", bloomBone)
+          } else {
+            console.log("No Bloom Bone found")
+          }
+        }
+      });
+  }
 }
 </script>
 
--- web/src/views/Mo.vue
+++ web/src/views/Mo.vue
@@ -2,6 +2,12 @@
   <a-scene>
     <!-- <a-sphere position="0 1.25 -5" radius="3.5" color="#EF2D5E"></a-sphere> -->
     <a-sky color="#ECECEC"></a-sky>
+    <a-assets>
+      <a-asset-item id="bromeliads-obj" src="./flowers/Bromeliads/Bromeliads.obj"></a-asset-item>
+      <a-asset-item id="bromeliads-mtl" src="./flowers/Bromeliads/Bromeliads.mtl"></a-asset-item>
+
+      <a-asset-item id="papatest-gltf" src="./flowers/papatest/papatest.gltf"></a-asset-item>
+    </a-assets>
     <Flower :stage="0" />
   </a-scene>
 </template>
