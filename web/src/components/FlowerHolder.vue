43e92369ce8959d1befa83cf07ab3e4e091a2f16
Jason Morris <jason@veergoggles.com>
2019-01-19 11:42:33 -0500

    Added flower stages to the flower

---
+++ web/src/components/FlowerHolder.vue
@@ -0,0 +1,51 @@
+<template>
+  <a-entity ref="rotationX" :rotation="`${offsetX} 0 0`">
+    <a-entity ref="rotationY" :rotation="`0 0 ${offsetY}`">
+      <a-entity :position="`0 ${hoverDistance} 0`">
+        <a-plane ref="flowerHolder" :material="`side: double; color: #FFFFFF; transparent: true; opacity: 1;`" width="1" height="2" src="img/flower_1_bloom.png"></a-plane>
+      </a-entity>
+    </a-entity>
+  </a-entity>
+</template>
+
+<script>
+// @ is an alias to /src
+export default {
+  name: 'FlowerHolder',
+  data(){
+      return {
+          hoverDistance: 0,
+          rotationX: 0,
+          rotationY: 0
+      }
+  },
+  props: {
+      planetRadius: {
+          type: Number,
+          default: 10,
+      },
+      offsetX: {
+          type: Number,
+          default: 0,
+      },
+      offsetY: {
+          type: Number,
+          default: 0,
+      },
+  },
+  computed: {
+      rotationX(){
+          return this.offsetX
+      },
+      rotationY(){
+          return this.offsetY
+      },
+  },
+  mounted(){
+      console.log('flowerHolder:', this.$refs.flowerHolder.object3D );
+
+      // Once we have the final object, work on the sizing
+      this.hoverDistance = this.planetRadius + (this.$refs.flowerHolder.getAttribute('height')) * 0.5;
+  },
+}
+</script>
--- web/src/components/Planet.vue
+++ web/src/components/Planet.vue
@@ -1,6 +1,9 @@
 <template>
     <a-sphere position="0 -7 -7" :radius="planetRadius" color="#EF2D5E">
         <b-flower :planetRadius="planetRadius" :offsetX="offsetX" :offsetY="offsetY"></b-flower>
+        <b-flower
+                v-for="(msg, idx) in msgs" :key="idx"
+                :planetRadius="planetRadius" :offsetX="offsetX" :offsetY="offsetY" :stage="randomStage(idx)"></b-flower>
     </a-sphere>
 </template>
 
@@ -16,6 +19,34 @@
                 offsetX: 0,
                 offsetY: 0,
                 welcomeText: 'Welcome to the home of the Bee',
+                msgs: [
+                    {
+                        name: 'Kai',
+                        msg: 'Get well soon!',
+                        flower: 0
+                    },
+                    {
+                        name: 'Mo',
+                        msg: 'Hope you feel better!',
+                        flower: 1
+                    },
+                    {
+                        name: 'Victoria',
+                        msg: 'Get well soon!',
+                        flower: 2
+                    },
+                    {
+                        name: 'Lydia',
+                        msg: 'See you soon!',
+                        flower: 2
+                    },
+                    {
+                        name: 'Jason',
+                        msg: 'Hope your insurance is paid up!',
+                        flower: 1
+                    },
+                ]
+
             }
         },
         components: {
@@ -27,6 +58,11 @@
             this.offsetY = Math.random() * rotation;
             console.log('this.offsetX:', this.offsetX);
         },
+        methods: {
+            randomStage(idx){
+                return idx % 3
+            },
+        }
 
     }
 </script>
