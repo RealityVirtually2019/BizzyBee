45e6c8ef3fd173dc412260686a27319cf120c5c2
Mo Kakwan <wavewash@gmail.com>
2019-01-18 21:00:26 -0500

    added in the first flower commit

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,13 +1,20 @@
 <template>
   <a-scene>
-    <a-sphere position="0 1.25 -5" radius="3.5" color="#EF2D5E"></a-sphere>
+    <!-- <a-sphere position="0 1.25 -5" radius="3.5" color="#EF2D5E"></a-sphere> -->
     <a-sky color="#ECECEC"></a-sky>
+    <Flower :stage="4"/>
   </a-scene>
 </template>
 
 <script>
 // @ is an alias to /src
+import Flower from '@/components/Flower'
+
 export default {
   name: 'Home',
+  components:{
+    Flower,
+  },
+
 }
 </script>
