edaf9577dc7ea060f45a14e4e856274fecc61812
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-20 14:29:13 +0000

    Deploy RealityVirtually2019/BizzyBee to github.com/RealityVirtually2019/BizzyBee.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.95dca4dcfab0a99cbc581bb268b620b7.js"
+  "precache-manifest.db473c7bcb3262ea7b233a856cf14073.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "web"});
