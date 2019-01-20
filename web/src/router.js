bffdc64e1cbe36746c8a44e102b8dbfc914ab2fb
ooookai <ooookai@gmail.com>
2019-01-20 02:10:44 -0500

    draft: replace router Home to Entry

--- web/src/router.js
+++ web/src/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+// import Home from './views/Home.vue'
 
 import Entry from './views/Entry.vue'
 import Build from './views/Build.vue'
