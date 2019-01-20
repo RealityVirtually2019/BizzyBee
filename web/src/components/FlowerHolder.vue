602d6e4a57b1368c8a5ad311c1bca33af34a3e2d
Mo Kakwan <wavewash@gmail.com>
2019-01-20 03:10:01 -0500

    trying to get the ar stuff to stop

--- web/src/components/FlowerHolder.vue
+++ web/src/components/FlowerHolder.vue
@@ -1,168 +1,172 @@
 <template>
-    <a-entity name='x-axis' :rotation="`${offsetX} 0 0`">
-        <a-entity name='y-axis' :rotation="`0 0 ${offsetY}`">
-            <a-entity name='planet-surface' :position="`0 ${hoverDistance} 0`">
-                <b-flower bounce-on-hover :stage="stage" :modelids="modelids" :mtlids="mtlids" :ps="flowerdata.moflower.ps"
-                          :play="play"></b-flower>
-                <!--<a-gltf-model name='flower-holder' ref="flowerHolder" src="models/example_flower.glb"></a-gltf-model>-->
-                <!--
+  <a-entity name='x-axis' :rotation="`${offsetX} 0 0`">
+    <a-entity name='y-axis' :rotation="`0 0 ${offsetY}`">
+      <a-entity name='planet-surface' :position="`0 ${hoverDistance} 0`">
+        <b-flower bounce-on-hover :stage="stage" :modelids="modelids"
+                  :mtlids="mtlids" :ps="flowerdata.moflower.ps"
+                  :play="play"></b-flower>
+        <!--<a-gltf-model name='flower-holder' ref="flowerHolder" src="models/example_flower.glb"></a-gltf-model>-->
+        <!--
                                         <a-plane name='flower-holder' ref="flowerHolder" :material="`side: double; color: #FFFFFF; transparent: true; opacity: 1;`"
                                                  :width="`${flowerWidth}`" :height="`${flowerHeight}`" :src="`${flowerUrl}`"></a-plane>
                         -->
-            </a-entity>
-        </a-entity>
+      </a-entity>
     </a-entity>
+  </a-entity>
 </template>
 
 <script>
-    import Flower from '@/components/Flower'
+import Flower from '@/components/Flower'
 
-    // @ is an alias to /src
-    export default {
-        name: 'FlowerHolder',
-        components: {
-            'b-flower': Flower,
+// @ is an alias to /src
+export default {
+  name: 'FlowerHolder',
+  components: {
+    'b-flower': Flower,
+  },
+  props: {
+    planetRadius: {
+      type: Number,
+      default: 10,
+    },
+    stage: {
+      type: Number,
+      default: 0,
+    },
+    type: {
+      type: Number,
+      default: 0,
+    },
+    rotX: {
+      type: Number,
+      default: 0,
+    },
+    rotY: {
+      type: Number,
+      default: 0,
+    },
+  },
+  data() {
+    return {
+      play: 'true',
+      hoverDistance: 0,
+      offsetX: 0,
+      offsetY: 0,
+      flowerUrl: '',
+      flowerPaths: {
+        0: [
+          'img/flowers/Blue Flower-1.png',
+          'img/flowers/Blue-Flower-2.png',
+          'img/flowers/Blue-Flower-3.png',
+        ],
+        1: [
+          'img/flowers/bulb-flower-1.png',
+          'img/flowers/bulb-flower-2.png',
+          'img/flowers/bulb-flower-3-blue.png',
+        ],
+        2: [
+          'img/flowers/flower_1_sprout.png',
+          'img/flowers/flower_1_sapling.png',
+          'img/flowers/flower_1_bloom.png',
+        ],
+      },
+      flowerHeight: 0,
+      flowerWidth: 0,
+      flowerdata: {
+        moflower: {
+          color: 0xff0000,
+          obj: ['#bromeliads-obj', '#papatest-gltf', '#monkey-gltf'],
+          mtl: ['#bromeliads-mtl', '#', '#'],
+          ps: [
+            'preset: dust; color: #EF0000,#44CC00; blending: 1; particleCount: 10',
+            'preset: dust; color: #000000,#FF0000; blending: 1; particleCount: 10',
+          ],
         },
-        props: {
-            planetRadius: {
-                type: Number,
-                default: 10,
-            },
-            stage: {
-                type: Number,
-                default: 0,
-            },
-            type: {
-                type: Number,
-                default: 0,
-            },
-            rotX: {
-                type: Number,
-                default: 0,
-            },
-            rotY: {
-                type: Number,
-                default: 0,
-            },
+        monkeyflower: {
+          color: 0x00ff00,
+          obj: ['#monkey'],
         },
-        data() {
-            return {
-                play: "true",
-                hoverDistance: 0,
-                offsetX: 0,
-                offsetY: 0,
-                flowerUrl: '',
-                flowerPaths: {
-                    0: [
-                        'img/flowers/Blue Flower-1.png',
-                        'img/flowers/Blue-Flower-2.png',
-                        'img/flowers/Blue-Flower-3.png',
-                    ],
-                    1: [
-                        'img/flowers/bulb-flower-1.png',
-                        'img/flowers/bulb-flower-2.png',
-                        'img/flowers/bulb-flower-3-blue.png',
-                    ],
-                    2: [
-                        'img/flowers/flower_1_sprout.png',
-                        'img/flowers/flower_1_sapling.png',
-                        'img/flowers/flower_1_bloom.png',
-                    ],
-                },
-                flowerHeight: 0,
-                flowerWidth: 0,
-                flowerdata: {
-                    moflower: {
-                        color: 0xff0000,
-                        obj: ["#bromeliads-obj", "#papatest-gltf", "#monkey-gltf"],
-                        mtl: ["#bromeliads-mtl", "#", "#" ],
-                        ps: ["preset: dust; color: #EF0000,#44CC00; blending: 1; particleCount: 10", "preset: dust; color: #000000,#FF0000; blending: 1; particleCount: 10"]
-                    },
-                    monkeyflower: {
-                        color: 0x00FF00,
-                        obj: ["#monkey"]
-                    }
-                }
-            }
-        },
-        computed: {
-            modelids() {
-                return this.flowerdata.moflower.obj
-            },
-            mtlids() {
-                return this.flowerdata.moflower.mtl
-            },
-            ps() {
-                return this.flowerdata.moflower.ps
-            }
-        },
-        mounted() {
-            // console.log('flowerHolder:', this.$refs.flowerHolder.object3D);
-
-            // Once we have the final object, work on the sizing
-            // this.hoverDistance = this.planetRadius + (this.$refs.flowerHolder.getAttribute('height')) * 0.5;
-            // this.hoverDistance = this.planetRadius + .65;
-            this.hoverDistance = this.planetRadius
-
-            // this.
-
-            // set the rotation of the objects
-            this.offsetX = this.rotX
-            this.offsetY = this.rotY
-
-            // set the flower type
-            // console.log('flower', this.$refs.flowerHolder.object3D,'stage:', this.type, this.stage);
-            this.flowerUrl = this.flowerPaths[this.type][this.stage]
-            switch (this.type) {
-                case 0:
-                    switch (this.stage) {
-                        case 0:
-                            this.flowerWidth = 0.5
-                            this.flowerHeight = 2
-                            break
-                        case 1:
-                            this.flowerWidth = 2
-                            this.flowerHeight = 4
-                            break
-                        case 2:
-                            this.flowerWidth = 3
-                            this.flowerHeight = 6
-                            break
-                    }
-                    break
-                case 1:
-                    switch (this.stage) {
-                        case 0:
-                            this.flowerWidth = 1
-                            this.flowerHeight = 2
-                            break
-                        case 1:
-                            this.flowerWidth = 1
-                            this.flowerHeight = 4
-                            break
-                        case 2:
-                            this.flowerWidth = 6
-                            this.flowerHeight = 6
-                            break
-                    }
-                    break
-                case 2:
-                    switch (this.stage) {
-                        case 0:
-                            this.flowerWidth = 1
-                            this.flowerHeight = 2
-                            break
-                        case 1:
-                            this.flowerWidth = 4
-                            this.flowerHeight = 4
-                            break
-                        case 2:
-                            this.flowerWidth = 3
-                            this.flowerHeight = 6
-                            break
-                    }
-                    break
-            }
-        },
+      },
     }
+  },
+  computed: {
+    modelids() {
+      return this.flowerdata.moflower.obj
+    },
+    mtlids() {
+      return this.flowerdata.moflower.mtl
+    },
+    ps() {
+      return this.flowerdata.moflower.ps
+    },
+  },
+  mounted() {
+    // console.log('flowerHolder:', this.$refs.flowerHolder.object3D);
+
+    // Once we have the final object, work on the sizing
+    // this.hoverDistance = this.planetRadius + (this.$refs.flowerHolder.getAttribute('height')) * 0.5;
+    // this.hoverDistance = this.planetRadius + .65;
+    this.hoverDistance = this.planetRadius
+
+    // this.
+
+    // set the rotation of the objects
+    this.offsetX = this.rotX
+    this.offsetY = this.rotY
+
+    // set the flower type
+    // console.log('flower', this.$refs.flowerHolder.object3D,'stage:', this.type, this.stage);
+    this.flowerUrl = this.flowerPaths[this.type][this.stage]
+    switch (this.type) {
+      case 0:
+        switch (this.stage) {
+          case 0:
+            this.flowerWidth = 0.5
+            this.flowerHeight = 2
+            break
+          case 1:
+            this.flowerWidth = 2
+            this.flowerHeight = 4
+            break
+          case 2:
+            this.flowerWidth = 3
+            this.flowerHeight = 6
+            break
+        }
+        break
+      case 1:
+        switch (this.stage) {
+          case 0:
+            this.flowerWidth = 1
+            this.flowerHeight = 2
+            break
+          case 1:
+            this.flowerWidth = 1
+            this.flowerHeight = 4
+            break
+          case 2:
+            this.flowerWidth = 6
+            this.flowerHeight = 6
+            break
+        }
+        break
+      case 2:
+        switch (this.stage) {
+          case 0:
+            this.flowerWidth = 1
+            this.flowerHeight = 2
+            break
+          case 1:
+            this.flowerWidth = 4
+            this.flowerHeight = 4
+            break
+          case 2:
+            this.flowerWidth = 3
+            this.flowerHeight = 6
+            break
+        }
+        break
+    }
+  },
+}
 </script>
--- web/src/views/Mo.vue
+++ web/src/views/Mo.vue
@@ -1,11 +1,13 @@
 <template>
+
   <a-scene>
-    <a-entity position="0 0 0">
-      <a-camera></a-camera>
-    </a-entity>
+    
+    <!--
     <a-sphere radius="1" color="#EF2D5E"></a-sphere>
     <a-sky color="#ECECEC"></a-sky>
+    
     <a-assets>
+      
       <a-asset-item id="Flower_3_bloom-dae" src="./flowers/Flower1/Flower_3_bloom.dae"></a-asset-item>
       <a-asset-item id="Flower_3_sapling-dae" src="./flowers/Flower1/Flower_3_sapling.dae"></a-asset-item>
       <a-asset-item id="Flower_3_sprout-dae" src="./flowers/Flower1/Flower_3_sprout.dae"></a-asset-item>
@@ -13,47 +15,61 @@
       <a-asset-item id="Flower_4_bloom-dae" src="./flowers/Flower1/Flower_4_bloom.dae"></a-asset-item>
       <a-asset-item id="Flower_4_sapling-dae" src="./flowers/Flower1/Flower_4_sapling.dae"></a-asset-item>
       <a-asset-item id="Flower_4_sprout-dae" src="./flowers/Flower1/Flower_4_sprout.dae"></a-asset-item>
+      
     </a-assets>
     <Flower :stage="stage" :modelids="flowerdata.flower1.obj" :mtlids="flowerdata.flower1.mtl" :ps="flowerdata.flower1.ps" :play="play"/>
+    -->
   </a-scene>
+  
 </template>
 
 <script>
-    // @ is an alias to /src
-    import Flower from '@/components/Flower'
+// @ is an alias to /src
+//import Flower from '@/components/Flower'
 
-let flowerdata =
-{
-  flower1:{
+let flowerdata = {
+  flower1: {
     color: 0xff0000,
-    obj:["#Flower_3_sprout-dae", "#Flower_3_sapling-dae", "Flower_3_bloom-dae"],
-    mtl:["#", "#", "#"],
-    ps:["color: #EF0000,#44CC00; blending: 1; particleCount: 10",
-        "color: #000000,#FF0000; blending: 1; particleCount: 100",
-        "color: #000000,#FF0000; blending: 1; particleCount: 100"]
+    obj: [
+      '#Flower_3_sprout-dae',
+      '#Flower_3_sapling-dae',
+      'Flower_3_bloom-dae',
+    ],
+    mtl: ['#', '#', '#'],
+    ps: [
+      'color: #EF0000,#44CC00; blending: 1; particleCount: 10',
+      'color: #000000,#FF0000; blending: 1; particleCount: 100',
+      'color: #000000,#FF0000; blending: 1; particleCount: 100',
+    ],
   },
-  flower2:{
+  flower2: {
     color: 0xff0000,
-    obj:["#Flower_4_sprout-dae", "#Flower_4_sapling-dae", "Flower_4_bloom-dae"],
-    mtl:["#", "#", "#"],
-    ps:["color: #EF0000,#44CC00; blending: 1; particleCount: 10",
-        "color: #000000,#FF0000; blending: 1; particleCount: 100",
-        "color: #000000,#FF0000; blending: 1; particleCount: 100"]
-  }
+    obj: [
+      '#Flower_4_sprout-dae',
+      '#Flower_4_sapling-dae',
+      'Flower_4_bloom-dae',
+    ],
+    mtl: ['#', '#', '#'],
+    ps: [
+      'color: #EF0000,#44CC00; blending: 1; particleCount: 10',
+      'color: #000000,#FF0000; blending: 1; particleCount: 100',
+      'color: #000000,#FF0000; blending: 1; particleCount: 100',
+    ],
+  },
 }
 
 export default {
-    name: 'Mo',
-    data() {
-        return {
-            flowerdata,
-            stage: 0,
-            play: "true",
-        }
-    },
-    components: {
-        'Flower': Flower,
-    },
+  name: 'Mo',
+  components: {
+    //Flower: Flower,
+  },
+  data() {
+    return {
+      flowerdata,
+      stage: 0,
+      play: 'true',
+    }
+  },
 }
 </script>
 
