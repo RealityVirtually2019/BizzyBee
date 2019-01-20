1f6d2ad13d69a5bdd46317879ec00595c7dff19b
ooookai <ooookai@gmail.com>
2019-01-19 19:34:56 -0500

    draft: update icons

--- web/src/App.vue
+++ web/src/App.vue
@@ -13,7 +13,7 @@ body {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
+  color: #4f2e7b;
 }
 #nav {
   padding: 30px;
