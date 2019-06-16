8280afa3ba035235c693784f2d5755cf22926090
ooookai <ooookai@gmail.com>
2019-06-17 02:33:40 +0800

    add new theme and scss vars

--- web/src/App.vue
+++ web/src/App.vue
@@ -17,4 +17,39 @@ body {
   width: 100%;
   height: 100%;
 }
+
+h1 {
+  font-size: 40px;
+  line-height: 40px;
+  letter-spacing: 0.04em;
+  color: $title;
+  font-weight: 200;
+  font-style: normal;
+}
+
+p,
+.formFill {
+  font-size: 15px;
+  line-height: 20px;
+  letter-spacing: 0.04em;
+  color: $normal;
+  font-weight: 400;
+  font-style: normal;
+}
+
+.formTitle {
+  font-size: 15px;
+  line-height: 18px;
+  letter-spacing: 0.04em;
+  color: $title;
+  font-weight: 700;
+  font-style: normal;
+}
+
+.btn {
+  color: white !important;
+  background-color: $secondary !important;
+  border-width: 0px !important;
+}
+
 </style>
--- web/vue.config.js
+++ web/vue.config.js
@@ -5,6 +5,16 @@ module.exports = {
     themeColor: '#4F2E7C',
   },
 
+  // https://css-tricks.com/how-to-import-a-sass-file-into-every-vue-component-in-an-app/
+
+  css: {
+    loaderOptions: {
+      sass: {
+        data: `@import "@/styles/_vars.scss";`,
+      },
+    },
+  },
+
   chainWebpack: config => {
     config.externals({
       AFRAME: 'AFRAME',
